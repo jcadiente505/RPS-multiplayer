b3d63ac436b3f3e4918dfdf3bf641489f77935cb
Cadman <jcadiente505@gmail.com>
2018-06-27 20:34:16 -0400

    work on the RPS selection/store into player object

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -19,7 +19,106 @@ var loseCounter = 0;
 var chatMsg = "";
 var playerTurn = 0;
 
-// grab the input values of user interface
-$("#name-input").val().trim()
-// set the player one info in firebase
+// database listener for values and player status
+
+database.ref("/players/").on("value", function(snapshot){
+
+  event.preventDefault();
+  // if statement too see assign player one/two status too user
+  if(snapshot.child("playerOne").exists()){
+    playerOne = snapshot.val().playerOne;
+    $("#player1-name").text(playerOne.name);
+  }
+  else {
+
+  }
+
+if(snapshot.child("playerTwo").exists()){
+  playerTwo = snapshot.val().playerTwo;
+  $("#player2-name").text(playerTwo.name);
+ 
+}
+else {
+
+}
+if(snapshot.child("playerOne").exists() && snapshot.child("playerTwo").exists()){
+
+    if (playerOne.choice === "blank" && playerTwo.choice === "blank") {
+
+    $("#game-state").text("Player One's turn!");
+
+    }
+    else if (playerOne.choice != "blank" && playerTwo.choice === "blank") {
+    $("game-state").text("Player Two's turn!")
+    }
+  }
+
+});
+
+// create listener for button values store them into firebase
+
+$("#name-button").on("click", function(){
+
+  event.preventDefault();
+
+// store the player name in variable
+  var playerName = $("#name-input").val().trim();
+// if statement to see which player you are
+if(playerOne === "false"){
+  playerOne = {
+    name: playerName,
+    wins: 0,
+    losses: 0,
+    choice: "blank"
+  };
+  alert("you are Player One");
+  database.ref().child("/players/playerOne").set(playerOne);
+}
+else if(playerTwo === "false"){
+  playerTwo = {
+    name: playerName,
+    wins: 0,
+    losses: 0,
+    choice: "blank"
+  };
+  alert("you are Player Two");
+  database.ref().child("/players/playerTwo").set(playerTwo);
+}
+else {
+  
+}
+console.log(playerName);
+
+  return false;
+})
+
+$(".rps-selector").on("click", function(button){
+
+  event.preventDefault();
+
+  if (playerOne != "false" && playerTwo != "false"){
+
+    if (playerOne.choice === "" && playerTwo.choice === "") {
+      $("#game-state-header").html("Player One's turn!");
+      console.log("player one turn");
+      playerOne.choice = $(this).attr("data-choice")
+      database.ref().child("/players/playerOne.choice").set(playerOne.choice);
+      console.log(playerOne.choice)
+    }
+    else if (playerOne.choice != "" && playerTwo.choice === "") {
+      $("game-state-header").text("Player Two's turn!");
+      console.log("player two turn");
+      playerTwo.choice = $(this).attr("data-choice")
+      database.ref().child("/players/playerTwo.choice").set(playerTwo.choice);
+    }
+  }
+  else {
+    $("#game-state-header").text("Waiting for Players")
+    console.log("player does not exist")
+  }
+  console.log(playerOne);
+  console.log(playerTwo);
+})
+
+
 
